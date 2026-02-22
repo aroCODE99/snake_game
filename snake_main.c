6b00908b40fe5465ae4068446c44184fb060ef86
aroCODE99 <prathamesh.pagare789@gmail.com>
2026-02-22 11:32:44 +0530

    Working snake game

--- snake_main.c
+++ snake_main.c
@@ -1,3 +1,5 @@
+// KITNA TIME WASTE KAREGA RE
+
 #include <stdio.h>
 #include <unistd.h>
 #include <assert.h>
@@ -20,17 +22,7 @@ enum DIRECTIONS {
 #define HEIGHT 600
 #define GRID_SIZE 40
 #define CURR_VERSION "1.0" // TODO: get the version from somewhere
-
-typedef struct {
-    int x;
-    int y;
-} Rect_Cords;
-
-typedef struct {
-    Rect_Cords cords;
-    size_t count;
-    size_t capacity;
-} Vector;
+#define SNAKE_CAP 10
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
@@ -40,13 +32,26 @@ bool quit = false;
 bool play = true;
 int grid_cols = WIDTH / GRID_SIZE;
 int grid_rows = HEIGHT / GRID_SIZE;
-int move_speed = 150; // in ms
+int move_speed = 200; // in ms
 
-bool collided(struct SDL_FRect *snake_rect, struct SDL_FRect *food_rect) {
-    if (snake_rect->x < food_rect->x + food_rect->w &&
-        snake_rect->y < food_rect->y + food_rect->h &&
-        snake_rect->y + snake_rect->h > food_rect->y &&
-        snake_rect->x + snake_rect->w > food_rect->x) {
+typedef struct {
+    int x;
+    int y;
+} Rect;
+
+// I think this should be the SDL_FRect
+typedef struct {
+    SDL_FRect *body; // this is the array
+    size_t length;
+    size_t capacity;
+} Snake;
+
+bool collided(struct SDL_FRect *snake_head, struct SDL_FRect *food)
+{
+    if (snake_head->x < food->x + food->w &&
+        snake_head->y < food->y + food->h &&
+        snake_head->y + snake_head->h > food->y &&
+        snake_head->x + snake_head->w > food->x) {
         return true;
     }
     return false;
@@ -60,11 +65,25 @@ int rand_int(int rand_max) {
     return rand() % rand_max;
 }
 
+// something is not working correctly
+void snake_move(Snake *snake, enum DIRECTIONS dir)
+{
+    for (int i = snake->length - 1; i > 0; --i) {
+        snake->body[i] = snake->body[i - 1];
+    }
+
+    // moving the snake_head
+    if (dir == U) snake->body[0].y -= vel * GRID_SIZE;
+    if (dir == D) snake->body[0].y += vel * GRID_SIZE;
+    if (dir == L) snake->body[0].x -= vel * GRID_SIZE;
+    if (dir == R) snake->body[0].x += vel * GRID_SIZE;
+}
+
 // TODO: Making the snake appear(Rendering multiple rects)
 int main(int argc, char **argv)
 {
-    int move_timer = 0; // what is this doing
     srand(time(0)); // seeding the random generator
+    int move_timer = 0;
 
     // parsing the arguments
     while (argc > 1) {
@@ -78,21 +97,23 @@ int main(int argc, char **argv)
         }
     }
 
-    Rect_Cords rect_cords = {
+    // x: 4, y: 4
+    Rect rect_cords = {
         .x = 4 * GRID_SIZE,
         .y = 4 * GRID_SIZE
     };
 
-    Rect_Cords food_cords = {
+    Rect food_cords = {
         .x = rand_int(grid_cols) * GRID_SIZE,
         .y = rand_int(grid_cols) * GRID_SIZE
     };
-    
+
     // now i need to do something here
     int screenWidth; int screenHeight;
 
-    enum DIRECTIONS movein = 1;
-
+    enum DIRECTIONS movein = 0;
+    printf("%d\n", movein);
+        
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -103,17 +124,24 @@ int main(int argc, char **argv)
         return SDL_APP_FAILURE;
     }
 
+    // initializing the snake
+    Snake snake;
+    snake.capacity = SNAKE_CAP;
+    snake.length = 3;
+    snake.body = (SDL_FRect *)malloc(sizeof(SDL_FRect) * SNAKE_CAP); 
+
+    assert(snake.body); // asserting if snake got allocated or not
+
+    snake.body[0].x = rect_cords.x;
+    snake.body[0].y = rect_cords.y;
+    snake.body[0].w = GRID_SIZE;
+    snake.body[0].h = GRID_SIZE;
+
     // Main gameloop
     while (!quit) {
         Uint64 start_time = SDL_GetPerformanceCounter();
-        float game_time = SDL_GetTicks() / 1000.0f;
-
-        SDL_FRect snake_rect;
-        snake_rect.x = rect_cords.x;
-        snake_rect.y = rect_cords.y;
-        snake_rect.w = GRID_SIZE;
-        snake_rect.h = GRID_SIZE;
-
+        /* float game_time = SDL_GetTicks() / 1000.0f; */
+        
         SDL_FRect food_rect;
         food_rect.x = food_cords.x;
         food_rect.y = food_cords.y;
@@ -123,11 +151,10 @@ int main(int argc, char **argv)
         // Event Loop
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_KEY_UP) {
-                if (event.key.key == SDLK_UP) movein = 0;
-                if (event.key.key == SDLK_DOWN) movein = 1;
-                if (event.key.key == SDLK_LEFT) movein = 2;
-                if (event.key.key == SDLK_RIGHT) movein = 3;
-                if (event.key.key == SDLK_SPACE) play = !play;
+                if (event.key.key == SDLK_UP)    movein = U;
+                if (event.key.key == SDLK_DOWN)  movein = D;
+                if (event.key.key == SDLK_LEFT)  movein = L;
+                if (event.key.key == SDLK_RIGHT) movein = R;
             }
             if (event.type == SDL_EVENT_QUIT) {
                 quit = true;
@@ -138,60 +165,54 @@ int main(int argc, char **argv)
         SDL_GetWindowSize(window, &screenWidth, &screenHeight); // Getting the windows size
 
         // Rendering the Stuff
+        // filling the bg color
         SDL_SetRenderDrawColorFloat(renderer, 0, 0, 0, 0); // first fill the bg
-        SDL_RenderClear(renderer); // this actually fillsX the color
+        SDL_RenderClear(renderer); // this actually fillsX the
         SDL_SetRenderDrawColorFloat(renderer, 255, 255, 255, 255); // first fill the bg
-        SDL_RenderFillRect(renderer, &snake_rect);
-        
+
         // TODO: improve this shit
         // I think this shit is true and i am totally assumming this shit is true
-        if (SDL_GetTicks() - move_timer > move_speed) {
-            if (movein == U) {
-                if (play) rect_cords.y -= vel * GRID_SIZE;
-            } else if (movein == D) {
-                if (play) rect_cords.y += vel * GRID_SIZE;
-            } else if (movein == R) {
-                if (play) rect_cords.x += vel * GRID_SIZE;
-            } else {
-                if (play) rect_cords.x -= vel * GRID_SIZE;      
-            }
-
+        if (SDL_GetTicks() - move_timer > move_speed && play) {
+            snake_move(&snake, movein);
             move_timer = SDL_GetTicks();
         }
 
         /* sleep(1); // just adding some time in-between the frames */
-        // now draw the random food here
-        SDL_SetRenderDrawColorFloat(renderer, 255, 0, 0, 0); // setting the color for the food
-        SDL_RenderFillRect(renderer, &food_rect);
         // Getting bounding correct
-        if (rect_cords.y + snake_rect.h > screenHeight || rect_cords.y < 0) {
-            SDL_Log("%lfs: Touched the Boundary", (start_time / SDL_GetPerformanceFrequency()) / 1000.0f);
+        SDL_FRect *head = &snake.body[0];
+
+        if (head->y < 0 || head->y + head->h > screenHeight ||
+            head->x < 0 || head->x + head->w > screenWidth) {
             quit = true;
         }
-
-        if (rect_cords.x + snake_rect.w > screenWidth || rect_cords.x < 0) {
-            SDL_Log("%lfs: Touched the Boundary", (start_time / SDL_GetPerformanceFrequency()) / 1000.0f);
-            quit = true;
-        }
-
-        if (collided(&snake_rect, &food_rect)) {
+        
+        if (collided(head, &food_rect)) {
+            snake.length += 1;
             food_cords.x = rand_int(grid_cols) * GRID_SIZE;
             food_cords.y = rand_int(grid_rows) * GRID_SIZE;
         }
 
+        // movement me hagg rha he ye mkc
+        for (int i = 0; i < snake.length; i++) {
+            SDL_RenderFillRect(renderer, &snake.body[i]);
+        }
+        // now draw the random food here
+        SDL_SetRenderDrawColorFloat(renderer, 255, 0, 0, 0); // setting the color for the food
+        SDL_RenderFillRect(renderer, &food_rect);
+
         SDL_RenderPresent(renderer);
 
         // after drawing do timing stuff
-        Uint64 end_time = SDL_GetPerformanceCounter();
+        /* Uint64 end_time = SDL_GetPerformanceCounter(); */
         // what is the frame delay?
         // frame_delay is the time that should be taken by the frame at 60fps
         // frame_delay is the target time per frame when running at 60 FPS :- ChatGpt
-        float frame_delay = (float) (1000.0f / move_speed);
+        /* float frame_delay = (float) (1000.0f / move_speed); */
 
         // frame_time is the time in ms taken by each frame in certain fps
         // SDL_GetPerformanceFrequency this usually converts the High Performance counter to time
-        double frame_time = (end_time - start_time) /
-            (double) SDL_GetPerformanceFrequency() * 1000.0f;
+        /* double frame_time = (end_time - start_time) / */
+        /*     (double) SDL_GetPerformanceFrequency() * 1000.0f; */
     }
 
     // cleaning of the code
