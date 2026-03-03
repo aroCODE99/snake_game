be6f605919d1fc71b7288c1ae132488498a3a8b5
aroCODE99 <prathamesh.pagare789@gmail.com>
2026-03-03 16:03:42 +0530

    Fixed snake growth and some minor bugs

--- snake_main.c
+++ snake_main.c
@@ -1,5 +1,3 @@
-// KITNA TIME WASTE KAREGA RE
-
 #include <stdio.h>
 #include <unistd.h>
 #include <assert.h>
@@ -22,7 +20,7 @@ enum DIRECTIONS {
 #define HEIGHT 600
 #define GRID_SIZE 40
 #define CURR_VERSION "1.0" // TODO: get the version from somewhere
-#define SNAKE_CAP 10
+#define SNAKE_LENGTH_CAP 4
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
@@ -32,21 +30,51 @@ bool quit = false;
 bool play = true;
 int grid_cols = WIDTH / GRID_SIZE;
 int grid_rows = HEIGHT / GRID_SIZE;
-int move_speed = 200; // in ms
+int move_speed = 300; // in ms
 
+// How do i use this
+// i still don't know how to use this shit
+// This is the Dynamic Array
 typedef struct {
-    int x;
-    int y;
-} Rect;
-
-// I think this should be the SDL_FRect
-typedef struct {
-    SDL_FRect *body; // this is the array
+    SDL_FRect *body; // Array of the Rects
     size_t length;
     size_t capacity;
 } Snake;
 
-bool collided(struct SDL_FRect *snake_head, struct SDL_FRect *food)
+float rand_float(void) {
+    return (float) rand() / (float) RAND_MAX;
+}
+
+int rand_int(int rand_max) {
+    return rand() % rand_max;
+}
+
+void init_snake(Snake *s, size_t initial_capacity)
+{
+    s->body = malloc(initial_capacity * sizeof(SDL_FRect));
+    s->capacity = initial_capacity;
+    s->length = 3;
+
+    // Initialize head position
+    s->body[0].x = 4 * GRID_SIZE;
+    s->body[0].y = 4 * GRID_SIZE;
+    s->body[0].w = GRID_SIZE;
+    s->body[0].h = GRID_SIZE;
+}
+
+void snake_append(Snake *s)
+{
+    if (s->length >= s->capacity) {
+        s->capacity *= 2;
+        s->body = realloc(s->body, s->capacity * sizeof(SDL_FRect));
+    }
+
+    // actually growing the snake
+    s->body[s->length] = s->body[s->length - 1];
+    s->length += 1;
+}
+
+bool food_collision(struct SDL_FRect *snake_head, struct SDL_FRect *food)
 {
     if (snake_head->x < food->x + food->w &&
         snake_head->y < food->y + food->h &&
@@ -57,21 +85,26 @@ bool collided(struct SDL_FRect *snake_head, struct SDL_FRect *food)
     return false;
 }
 
-float rand_float(void) {
-    return (float) rand() / (float) RAND_MAX;
+bool snake_collision(Snake *snake)
+{
+    SDL_FRect head = snake->body[0];
+    for (size_t i = 1; i < snake->length; ++i) {
+        if (head.y == snake->body[i].y && head.x == snake->body[i].x) {
+            return true;
+        }
+    }
+    return false;
 }
 
-int rand_int(int rand_max) {
-    return rand() % rand_max;
-}
-
-// something is not working correctly
+// TODO: Movement in opp direction should not be allowed
 void snake_move(Snake *snake, enum DIRECTIONS dir)
 {
-    for (int i = snake->length - 1; i > 0; --i) {
+    // This is the smartest logic that i have seen
+    // when head moves, automatically the whole snake moves
+    for (size_t i = snake->length - 1; i > 0; --i) {
         snake->body[i] = snake->body[i - 1];
     }
-
+    
     // moving the snake_head
     if (dir == U) snake->body[0].y -= vel * GRID_SIZE;
     if (dir == D) snake->body[0].y += vel * GRID_SIZE;
@@ -79,12 +112,33 @@ void snake_move(Snake *snake, enum DIRECTIONS dir)
     if (dir == R) snake->body[0].x += vel * GRID_SIZE;
 }
 
+void free_snake(Snake *s) {
+    free(s->body);
+    s->body = NULL;
+}
+
+bool check_food_appearing_on_snake(Snake *s, SDL_FRect *f)  {
+    for (size_t i = 0; i < s->length; ++i) {
+        if (s->body[i].x == f->x && s->body[i].y == f->y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void move_food(Snake *s, SDL_FRect *f) {
+    do {
+        f->x = rand_int(grid_cols) * GRID_SIZE;
+        f->y = rand_int(grid_rows) * GRID_SIZE;
+    } while (check_food_appearing_on_snake(s, f));
+}
+
 // TODO: Making the snake appear(Rendering multiple rects)
 int main(int argc, char **argv)
 {
     srand(time(0)); // seeding the random generator
     int move_timer = 0;
-
+    
     // parsing the arguments
     while (argc > 1) {
         char *curr_arg = SHIFT_ARGS(argc, argv);
@@ -97,23 +151,11 @@ int main(int argc, char **argv)
         }
     }
 
-    // x: 4, y: 4
-    Rect rect_cords = {
-        .x = 4 * GRID_SIZE,
-        .y = 4 * GRID_SIZE
-    };
-
-    Rect food_cords = {
-        .x = rand_int(grid_cols) * GRID_SIZE,
-        .y = rand_int(grid_cols) * GRID_SIZE
-    };
-
     // now i need to do something here
     int screenWidth; int screenHeight;
 
-    enum DIRECTIONS movein = 0;
-    printf("%d\n", movein);
-        
+    enum DIRECTIONS movein = 3; // moving right first
+    
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -123,30 +165,22 @@ int main(int argc, char **argv)
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-
-    // initializing the snake
+    
+    SDL_FRect food_rect;
+    food_rect.x = rand_int(grid_cols) * GRID_SIZE;
+    food_rect.y = rand_int(grid_rows) * GRID_SIZE;
+    food_rect.w = GRID_SIZE;
+    food_rect.h = GRID_SIZE;
+    
+    // Initializing the Snake struct
     Snake snake;
-    snake.capacity = SNAKE_CAP;
-    snake.length = 3;
-    snake.body = (SDL_FRect *)malloc(sizeof(SDL_FRect) * SNAKE_CAP); 
-
-    assert(snake.body); // asserting if snake got allocated or not
-
-    snake.body[0].x = rect_cords.x;
-    snake.body[0].y = rect_cords.y;
-    snake.body[0].w = GRID_SIZE;
-    snake.body[0].h = GRID_SIZE;
+    init_snake(&snake, SNAKE_LENGTH_CAP);
+    assert(snake.body); // asserting the snake
 
     // Main gameloop
     while (!quit) {
-        Uint64 start_time = SDL_GetPerformanceCounter();
+        /* Uint64 start_time = SDL_GetPerformanceCounter(); */
         /* float game_time = SDL_GetTicks() / 1000.0f; */
-        
-        SDL_FRect food_rect;
-        food_rect.x = food_cords.x;
-        food_rect.y = food_cords.y;
-        food_rect.w = GRID_SIZE;
-        food_rect.h = GRID_SIZE;
 
         // Event Loop
         while (SDL_PollEvent(&event)) {
@@ -155,6 +189,7 @@ int main(int argc, char **argv)
                 if (event.key.key == SDLK_DOWN)  movein = D;
                 if (event.key.key == SDLK_LEFT)  movein = L;
                 if (event.key.key == SDLK_RIGHT) movein = R;
+                if (event.key.key == SDLK_SPACE) play = !play;
             }
             if (event.type == SDL_EVENT_QUIT) {
                 quit = true;
@@ -170,14 +205,14 @@ int main(int argc, char **argv)
         SDL_RenderClear(renderer); // this actually fillsX the
         SDL_SetRenderDrawColorFloat(renderer, 255, 255, 255, 255); // first fill the bg
 
-        // TODO: improve this shit
-        // I think this shit is true and i am totally assumming this shit is true
-        if (SDL_GetTicks() - move_timer > move_speed && play) {
+        if ((int)SDL_GetTicks() - move_timer > move_speed && play) {
             snake_move(&snake, movein);
+            if (snake_collision(&snake)) {
+                quit = true;
+            }
             move_timer = SDL_GetTicks();
         }
-
-        /* sleep(1); // just adding some time in-between the frames */
+        
         // Getting bounding correct
         SDL_FRect *head = &snake.body[0];
 
@@ -185,18 +220,18 @@ int main(int argc, char **argv)
             head->x < 0 || head->x + head->w > screenWidth) {
             quit = true;
         }
-        
-        if (collided(head, &food_rect)) {
-            snake.length += 1;
-            food_cords.x = rand_int(grid_cols) * GRID_SIZE;
-            food_cords.y = rand_int(grid_rows) * GRID_SIZE;
-        }
 
-        // movement me hagg rha he ye mkc
-        for (int i = 0; i < snake.length; i++) {
+        // TODO: Fix the food appearing on the snake
+        if (food_collision(head, &food_rect)) {
+            snake_append(&snake);
+            move_food(&snake, &food_rect);
+        }
+        
+        // Rendering the snake
+        for (size_t i = 0; i < snake.length; ++i) {
             SDL_RenderFillRect(renderer, &snake.body[i]);
         }
-        // now draw the random food here
+
         SDL_SetRenderDrawColorFloat(renderer, 255, 0, 0, 0); // setting the color for the food
         SDL_RenderFillRect(renderer, &food_rect);
 
@@ -218,5 +253,6 @@ int main(int argc, char **argv)
     // cleaning of the code
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    free_snake(&snake); // freeing the snake.bodyg
     return 0;
 }
